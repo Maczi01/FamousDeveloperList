b00366519803fa4f3c3fc0db9379a1cb087ce7f1
Maczi01 <mateusz.w.twardy@gmail.com>
2020-03-21 23:32:21 +0100

    add my component with state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ListWrapper from "./components/ListWrapper/ListWrapper";
 import './index.css';
-
+import MyComponent from "./MyComponent"
 
 const App = () => (
     <div>
+        <MyComponent/>
         <ListWrapper/>
     </div>
 );
