bb8627290466dba274e20a0dbbf8b6daa612b5cc
Maczi01 <mateusz.w.twardy@gmail.com>
2020-03-23 21:45:52 +0100

    add style to button

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import styles from './Title.module.scss'
+import styles from './Title.module.scss';
 
-const Title = ({children}) =>(
-    <h1 className={styles.title}>{children}</h1>
+const Title = ({children}) => (
+  <h2 className={styles.title}>
+    {children}
+  </h2>
 );
 
 export default Title;
